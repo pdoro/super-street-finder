327d5b490c5d5f35aaa1d40be5644e53112ec7fc
Pablo Domingo <pablo.doro92@gmail.com>
2021-02-24 21:20:13 +0100

    Added sample query

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 echo 'Starting elasticsearch'
 docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.10.1
+
 sleep 10
+
 echo 'Starting super-street-finder'
-mvn spring-boot:run -DskipTests
+mvn spring-boot:run -DskipTests &
+
+sleep 15
+
+curl --location --request GET 'http://localhost:8080/search' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "input": "Calle de Pugcerd"
+}   '
